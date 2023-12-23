c8cb9fc75e367f9802d95fa57b7c99f43984afcf
Calvin2328 <101858528+Calvin2328@users.noreply.github.com>
2023-12-23 14:49:59 +0800

    first commit

---
+++ src/views/WelcomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="welcome container">
+    <p>welcome</p>
+    hi
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.welcome {
+  text-align: center;
+  padding: 20px 0;
+}
+</style>
